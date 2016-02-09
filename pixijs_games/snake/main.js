c102549b8a56427f1749781d319eb12b55884b8a
Joilnen Batista Leite <joilnen@gmail.com>
2016-02-09 05:21:29 -0300

    getting keyboard event register with document event listener

--- pixijs_games/snake/main.js
+++ pixijs_games/snake/main.js
@@ -1,39 +1,45 @@
 (function() {
-var width = 480;
-var height = 480;
-var figs = 'figs/';
+    var width = 480;
+    var height = 480;
+    var figs = 'figs/';
 
+    var stage = new PIXI.Stage(0x00000);
 
-var stage = new PIXI.Stage(0x00000);
+    var renderer = new PIXI.CanvasRenderer(width, height);
+    document.body.appendChild(renderer.view);
 
-var renderer = new PIXI.CanvasRenderer(width, height);
-document.body.appendChild(renderer.view);
+    var snake = new PIXI.Sprite(
+           PIXI.Texture.fromImage(figs + "sn.png") 
+    );
 
-var snake = new PIXI.Sprite(
-       PIXI.Texture.fromImage(figs + "sn.png") 
-);
+    snake.anchor.x = .5;
+    snake.anchor.y = .5;
+    snake.position.x = width / 2;
+    snake.position.y = height / 2;
 
-snake.anchor.x = .5;
-snake.anchor.y = .5;
-snake.position.x = width / 2;
-snake.position.y = height / 2;
+    stage.addChild(snake);
 
-stage.addChild(snake);
+    document.addEventListener('keydown', onKeyDown);
 
-document.addEventListener('keydown', onKeyDown);
-
-requestAnimationFrame(animate);
-function animate() {
     requestAnimationFrame(animate);
-    // snake.x += .5;
-    renderer.render(stage);
-}
+    function animate() {
+        requestAnimationFrame(animate);
+        // snake.x += .5;
+        renderer.render(stage);
+    }
 
-function onKeyDown(key) {
-    if(key.keyCode === 37)
-        snake.x -= 0.1;
-    if(key.keyCode === 39)
-        snake.x += 0.1;
-    console.log('Pressed key');
-}
+    function onKeyDown(key) {
+        if(key.keyCode === 37)
+            snake.x -= 3;
+        if(key.keyCode === 39)
+            snake.x += 3;
+        if(key.keyCode === 38)
+            snake.y -= 3;
+        if(key.keyCode === 40)
+            snake.y += 3;
+
+        console.log('Pressed key');
+    }
 })();
+
+
