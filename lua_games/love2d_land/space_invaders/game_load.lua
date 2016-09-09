56fe66bea5a7f71f74216371613169d555bd5a2d
Joilnen <joilnen@gmail.com>
2016-09-09 10:42:27 -0300

    made background 'black' of sprite transparent really cool

--- lua_games/love2d_land/space_invaders/game_load.lua
+++ lua_games/love2d_land/space_invaders/game_load.lua
@@ -8,6 +8,16 @@ animator_list = {}
 
 function loadAssets(cannon, barries)
     enemies = love.graphics.newImage(figs_dir .. 'enemies.png')
+
+    -- Isso foi muito fooooda por mim mesmo heh 
+    enemies:getData():mapPixel(
+        function (x, y, r, g, b, a) 
+            if r == 0 and g == 0 and b == 0 then
+                a = 0
+            end
+            return r, g, b, a
+        end
+     )
 end
 
 function setAnimator()
